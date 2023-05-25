216150665e4bbe5ebc04a3c9003c0bd30c34307f
AloneFancy <123137230+Giovinezza@users.noreply.github.com>
2023-05-26 05:09:48 +0700

    0.1

--- convert.py
+++ convert.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 def output_html_code(html):
-    return BeautifulSoup(html).get_text()
+    return BeautifulSoup(html,features="lxml").get_text()
 
 def RSTfile(data):
     Raw_file="\n"
     Raw_file+="="*len(data["Module Name"]) + "\n"
     Raw_file+=data["Module Name"] + "\n"
     Raw_file+="="*len(data["Module Name"]) + "\n"
+    
     for scope in data["List Artifact Info"]:
-        Raw_file +=  output_html_code(scope["ReqIF.Text"])  +"\n"
+        pass
+        #Raw_file+="\n" +data["Title"] + '\n' + len(data["Title"])*'*'
+        #Raw_file += str(scope["ReqIF.Name"])  +"\n"
     return Raw_file
 
 def return_key(long_name):
@@ -16,15 +19,23 @@ def return_key(long_name):
         return 'Contributor'
     elif long_name =='ReqIF.ForeignCreatedBy':
         return 'Creator'
+    elif long_name =='ReqIF.ForeignCreatedOn':
+        return 'Created On'
     elif long_name =='ReqIF.ForeignID':
         return 'Identifier'
-    elif long_name =='ReqIF.ChapterName':
+    elif long_name =='ReqIF.Name':
         return 'Title'
+    elif long_name =="ReqIF.Description":return None
+    elif long_name=="Artifact Format":return None
+    elif long_name=="ReqIF.ForeignModifiedOn":return None
+    elif long_name=="ReqIF.ChapterName":return None
     return long_name
 
 def process_value(key,value):
     if key=='Identifier':
         return int(value)
-    elif key=='ReqIF.ChapterName':
+    elif key=='Title':
         return output_html_code(value)
+    elif key==None:
+        pass
     return value
